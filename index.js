3655cfa7bfc81a2292a61f89602fcfe981c1584f
alberto <albon_marvel@hotmail.com>
2019-06-01 17:59:25 -0500

    VAR PORT

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://albon2290:123456789_@cluster0-e1lgw.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
 	.then(() => {
 		console.log('Conexxion exitosa');
-		app.listen(port, () =>{
+		app.listen(process.env.PORT || port, () =>{
 			console.log("servido ok");
 		});
 	}).catch(err => console.log(err));
