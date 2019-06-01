df46f52355a654c89b6aacd91015278a071b6a78
alberto <albon_marvel@hotmail.com>
2019-06-01 18:02:25 -0500

    show port

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb+srv://albon2290:123456789_@cluster0-e1lgw.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
 	.then(() => {
-		console.log('Conexxion exitosa');
-		app.listen(process.env.PORT || port, () =>{
+		let puerto = process.env.PORT || port;
+		console.log('Conexxion exitosa en puerto: ' + puerto );
+		app.listen(puerto, () =>{
 			console.log("servido ok");
 		});
 	}).catch(err => console.log(err));
